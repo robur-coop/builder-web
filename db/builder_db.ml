3fe8bcb9979f36a1a29a1539cb20b8a741a9245d
Reynir Björnsson <reynir@reynir.dk>
2023-09-11 12:31:11 +0200

    Add migration for modified index

--- db/builder_db.ml
+++ db/builder_db.ml
@@ -5,7 +5,7 @@ open Caqti_request.Infix
 let application_id = 1234839235l
 
 (* Please update this when making changes! *)
-let current_version = 16L
+let current_version = 17L
 
 type 'a id = 'a Rep.id
 
