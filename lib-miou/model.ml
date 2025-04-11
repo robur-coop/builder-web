868adebf372282d8b1b56f9b026ce9a09fcd6e19
Calascibetta Romain <romain.calascibetta@gmail.com>
2025-04-11 17:18:06 +0200

    Continue

--- lib-miou/model.ml
+++ lib-miou/model.ml
@@ -19,3 +19,34 @@ let build_hash hash (module Db : CONN) =
 let build_exists uuid (module Db : CONN) =
   let+ opt = Db.find_opt Builder_db.Build.get_by_uuid uuid in
   Option.is_some opt
+
+let builds_grouped_by_output job_id platform (module Db : CONN) =
+  let* lst = Db.collect_list Builder_db.Build.get_all_artifact_sha (job_id, platform) in
+  let fn acc hash = match acc with
+    | Error _ as err -> err
+    | Ok builds ->
+        let* build = Db.find Builder_db.Build.get_with_main_binary_by_hash hash in
+        Ok (build :: builds) in
+  let* lst = List.fold_left fn (Ok []) lst in
+  Ok (List.rev lst)
+
+let builds_grouped_by_output_with_failed job_id platform ((module Db : CONN) as db) =
+  let+ builds = builds_grouped_by_output job_id platform db in
+  let+ failed = Db.collect_list Builder_db.Build.get_failed_builds (job_id, platform) in
+  let failed = List.map (fun b -> b, None) failed in
+  let cmp (a, _) (b, _) = Ptime.compare b.Builder_db.Build.start a.Builder_db.Build.start in
+  List.merge cmp builds failed
+
+let not_found = function
+  | None -> Error `Not_found
+  | Some v -> Ok v
+
+let job_id job_name (module Db : CONN) =
+  Db.find_opt Builder_db.Job.get_id_by_name job_name
+
+let job_and_readme job (module Db : CONN) =
+  let* job_id = job_id job (module Db) in
+  let* job_id = not_found job_id in
+  let* readme_id = Db.find Builder_db.Tag.get_id_by_name "readme.md" in
+  let* readme = Db.find_opt Builder_db.Job_tag.get_value (readme_id, job_id) in
+  Ok (job_id, readme)
